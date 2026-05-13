3a0e9dbeef395627d82b491ffe7a8a0e12e0267b
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-13 10:26:03 -0600

    Add files via upload

---
+++ project1_revenue_margin/Running monthly revenue by month.sql
@@ -0,0 +1,13 @@
+/* Running monthly revenue by month */
+
+WITH monthly AS(
+	SELECT	DATE_TRUNC('month', o.order_Date) AS month,
+			SUM(oi.unit_price * oi.quantity - oi.discount_amount) AS revenue
+	FROM orders o
+	JOIN order_items oi ON o.order_id = oi.order_id
+	GROUP BY DATE_TRUNC('month', o.order_Date)
+)
+SELECT	month,
+		revenue,
+		SUM(revenue) OVER(ORDER BY month) AS running_revenue
+FROM monthly;
