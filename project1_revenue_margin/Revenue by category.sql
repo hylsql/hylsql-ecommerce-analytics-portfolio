feefbbdded48b64bb54d19950a5a647e48c07279
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-13 10:30:46 -0600

    Delete project1_revenue_margin/Revenue by category.sql

--- project1_revenue_margin/Revenue by category.sql
+++
@@ -1,9 +0,0 @@
-/* Revenue by sales channel */
-
-SELECT	o.sales_channel,
-		SUM(oi.unit_price * oi.quantity - oi.discount_amount) AS revenue
-FROM orders o
-JOIN order_items oi ON o.order_id = oi.order_id
-GROUP BY o.sales_channel
-ORDER BY revenue DESC;
-
