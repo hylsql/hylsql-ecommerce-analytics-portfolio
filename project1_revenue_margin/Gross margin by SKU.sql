f225104876f8c24e25ac5ede2713f17667ad8084
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-15 21:21:21 -0600

    Update Gross margin by SKU.sql

--- project1_revenue_margin/Gross margin by SKU.sql
+++ project1_revenue_margin/Gross margin by SKU.sql
@@ -2,9 +2,9 @@
 
 SELECT	p.sku,
 		SUM(oi.unit_price * oi.quantity - oi.discount_amount) AS revenue,
-		SUM(p.standard_cost) AS cost,
+		SUM(p.standard_cost * oi.quantity) AS cost,
 		SUM(oi.unit_price * oi.quantity - oi.discount_amount - p.standard_cost) AS gross_margin
 FROM products p
-JOIN order_items oi ON p.product_id = oi.order_item_id
+JOIN order_items oi ON p.product_id = oi.product_id
 GROUP BY p.sku
 ORDER BY gross_margin DESC;
