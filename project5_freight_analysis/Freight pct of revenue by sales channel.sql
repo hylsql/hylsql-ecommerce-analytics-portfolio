799b7d573337ec3ae0f8d667813e77d916b39c27
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-16 16:16:03 -0600

    Update Freight pct of revenue by sales channel.sql

--- project5_freight_analysis/Freight pct of revenue by sales channel.sql
+++ project5_freight_analysis/Freight pct of revenue by sales channel.sql
@@ -1,13 +1,25 @@
 /* Freight % of revenue by sales channel */
 
+WITH order_revenue AS (
+    SELECT
+        order_id,
+        SUM(quantity * unit_price - discount_amount) AS revenue
+    FROM order_items
+    GROUP BY order_id
+),
+	order_frights AS(
+	SELECT	order_id,
+	SUM(freight_cost) AS freight_cost
+    FROM shipments
+    GROUP BY order_id
+	)
 SELECT	o.sales_channel,
-		SUM(s.freight_cost) AS shipping_cost,
-		SUM(oi.unit_price * oi.quantity - oi.discount_amount) AS revenue,
-		SUM(s.freight_cost) * 100.0
-		/ NULLIF(SUM(oi.unit_price * oi.quantity - oi.discount_amount),0) AS shipping_pct
+		SUM(ofr.freight_cost) AS shipping_cost,
+		SUM(orv.revenue) AS revenue,
+		SUM(ofr.freight_cost) / NULLIF(SUM(orv.revenue), 0) AS freight_pct
 FROM orders o
-JOIN order_items oi ON o.order_id = oi.order_id
-JOIN shipments s ON s.order_id = o.order_id
+JOIN order_revenue orv ON o.order_id = orv.order_id
+JOIN order_frights ofr ON ofr.order_id = o.order_id
 GROUP BY o.sales_channel
-ORDER BY shipping_pct DESC;
+ORDER BY shipping_cost DESC;
 
