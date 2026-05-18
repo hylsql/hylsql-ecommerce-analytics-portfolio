e08cd2b79d0cd6630d8da2c380e4beaf7ae2ff35
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-17 23:23:23 -0600

    Update Slow-moving SKUs.sql

--- project2_inventory_optimization/Slow-moving SKUs.sql
+++ project2_inventory_optimization/Slow-moving SKUs.sql
@@ -15,11 +15,11 @@ inventory_summary AS(
 SELECT	p.sku,
 		p.product_name,
 		p.category,
-		COALESCE(s.units_sold,0) AS units_sold,
-		COALESCE(i.inventory_units,0) AS inventory_units,
-		COALESCE(i.inventory_units,0) * p.standard_cost AS inventory_value
+		COALESCE(s.units_sold, 0) AS units_sold,
+		COALESCE(i.inventory_units, 0) AS inventory_units,
+		COALESCE(i.inventory_units, 0) * p.standard_cost AS inventory_value
 FROM products p
 LEFT JOIN inventory_summary i ON i.product_id = p.product_id
 LEFT JOIN sales s ON s.product_id = p.product_id
-WHERE COALESCE(i.inventory_units,0) > 10 AND COALESCE(s.units_sold,0) < 10
+WHERE COALESCE(i.inventory_units, 0) > 10 AND COALESCE(s.units_sold, 0) < 10
 ORDER BY inventory_value DESC;
